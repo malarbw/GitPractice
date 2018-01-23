f0da1b17dbf6bfa0024a5a3bd80ef62d45f2ef57
malarbw <malarbw@gmail.com>
2018-01-23 14:30:54 -0500

    first commit

---
+++ src/NewGitDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/*
+ * Brad Malarkey
+ * 
+ * Simple program to practice pushing to github
+ */
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String firstName;
+		System.out.println("Enter your first name: ");
+		firstName = input.nextLine();
+		System.out.println("First name is: " + firstName);
+
+	}
+
+}
